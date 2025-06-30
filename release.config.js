bd0e814b5f402d09c6a7f30b02e50dd0a242ccbe
Lakshmi Sriramulu Chennareddy-halliburton.com <lakshmi.sriramulu106@U-1SF36U7SA3NCG.cz1cg1.pri>
2025-06-30 20:10:18 +0530

    semantic-release poc

---
+++ release.config.js
@@ -0,0 +1,29 @@
+// release.config.js
+
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "mvn versions:set -DnewVersion=${nextRelease.version}"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["pom.xml", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ],
+    [
+      "@semantic-release/github",
+      {
+        successComment: false,
+        failComment: false
+      }
+    ]
+  ]
+};
