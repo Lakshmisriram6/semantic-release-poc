ee972902df49b19405de932feb68513fe0ce55be
Lakshmi Sriramulu Chennareddy-halliburton.com <lakshmi.sriramulu106@U-1SF36U7SA3NCG.cz1cg1.pri>
2025-06-30 22:59:10 +0530

    test

--- release.config.js
+++ release.config.js
@@ -3,14 +3,21 @@
 module.exports = {
   branches: ["main"],
   plugins: [
+    // Analyze commit messages
     "@semantic-release/commit-analyzer",
+
+    // Generate changelog.md
     "@semantic-release/release-notes-generator",
+
+    // Update pom.xml version via Maven
     [
       "@semantic-release/exec",
       {
         prepareCmd: "mvn versions:set -DnewVersion=${nextRelease.version}"
       }
     ],
+
+    // Commit updated files and push tag
     [
       "@semantic-release/git",
       {
@@ -18,6 +25,8 @@ module.exports = {
         message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
     ],
+
+    // Create GitHub Release
     [
       "@semantic-release/github",
       {
