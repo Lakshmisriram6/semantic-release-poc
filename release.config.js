b96a00f632c41574b1ee09f16fb19a53aaa3fe91
Lakshmi Sriramulu Chennareddy-halliburton.com <lakshmi.sriramulu106@U-1SF36U7SA3NCG.cz1cg1.pri>
2025-07-01 10:13:49 +0530

    change

--- release.config.js
+++ release.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   branches: ["main"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          { type: "feat", release: "minor" },          // features bump minor
+          { type: "fix", release: "patch" },           // fixes bump patch
+          { type: "chore", release: "patch" },         // chore bumps patch too
+          { breaking: true, release: "major" }         // breaking change bumps major
+        ]
+      }
+    ],
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/exec",
