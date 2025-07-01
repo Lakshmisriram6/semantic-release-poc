1b6a25be8a0b7a2088ec75b255a119de14cf44d3
Lakshmi Sriramulu Chennareddy-halliburton.com <lakshmi.sriramulu106@U-1SF36U7SA3NCG.cz1cg1.pri>
2025-07-01 09:06:33 +0530

    feat: add greetUser method to demonstrate minor release

--- src/main/java/com/lakshmi/example/demo/DemoApplication.java
+++ src/main/java/com/lakshmi/example/demo/DemoApplication.java
@@ -6,9 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("hello semantic release");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("hello semantic release");
+        greetUser(); // New feature added
+    }
 
+    private static void greetUser() {
+        System.out.println("This is a minor feature update for semantic-release.");
+    }
 }
