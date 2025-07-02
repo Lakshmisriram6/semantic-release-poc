634761d9234c3d1102071f5b4cc8f5368956dc96
Lakshmi Sriramulu Chennareddy-halliburton.com <lakshmi.sriramulu106@U-1SF36U7SA3NCG.cz1cg1.pri>
2025-07-02 15:23:09 +0530

    initial release

--- src/main/java/com/lakshmi/example/demo/DemoApplication.java
+++ src/main/java/com/lakshmi/example/demo/DemoApplication.java
@@ -9,16 +9,10 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("hello semantic release");
-        greetNewUser();  // New feature added
-		hello();
+       
 		
     }
 
-    private static void greetNewUser() {
-        System.out.println("Welcome to the minor release demo!");
-    }
-	 private static void hello() {
-        System.out.println("Welcome to the minor release demo!");
-    }
+    
 	
 }
