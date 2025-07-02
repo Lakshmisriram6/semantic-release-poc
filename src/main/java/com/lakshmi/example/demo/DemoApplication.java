eaa5ba2a24743009c3075bde82a07fd7a64c1adf
Lakshmi Sriramulu Chennareddy-halliburton.com <lakshmi.sriramulu106@U-1SF36U7SA3NCG.cz1cg1.pri>
2025-07-02 13:19:35 +0530

    delete methods

--- src/main/java/com/lakshmi/example/demo/DemoApplication.java
+++ src/main/java/com/lakshmi/example/demo/DemoApplication.java
@@ -9,16 +9,5 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("hello semantic release");
-        greetUser("Lakshmi");    // BREAKING CHANGE: now requires a name
-        greetUser2();
-    }
-
-    // 🔺 BREAKING CHANGE: changed method to require a String argument
-    private static void greetUser(String name) {
-        System.out.println("Hello, " + name + "! This is a major version change.");
-    }
-
-    private static void greetUser2() {
-        System.out.println("This is a patch-level fix for semantic-release.");
-    }
+	}
 }
